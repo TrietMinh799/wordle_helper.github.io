1ac039dcb122f69d1f17a5ba8d6bd65ea01f16fc
TrietMinh799 <luckyblockblack@gmail.com>
2026-05-19 23:08:52 +0700

    Add the header for the web

--- src/App.jsx
+++ src/App.jsx
@@ -129,6 +129,7 @@ function App() {
   return (
     <div className="App">
       <div className="guessing">
+        <h1>Wordle Helper</h1>
         <div className="correct-container">
           <p className="header">Correct characters: </p>
           <div className="input-row">
